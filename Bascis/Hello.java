ddc42f5c5ffa89389358195d5f675ea38774383c
unknown <vikashkumar555550@gmail.com>
2024-10-21 09:15:01 +0530

    add new file

---
+++ Bascis/Hello.java
@@ -0,0 +1,8 @@
+ package Bascis;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello vikash");
+        System.out.println("hello vikash kaha ho bahi ,may to bha r hu abhi");
+    }
+}
---
+++ Bascis/printhello.java
@@ -0,0 +1,9 @@
+package Bascis;
+
+public class printhello {
+    public static void main(String[] args) {
+        System.out.print("this side vikash kuma \n");
+        System.out.println("hello deva");
+    }
+    
+}
